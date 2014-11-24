756e472d5f14d3b3f0a6c3f2b86593497f81786c
conorhastings <hastings.conorm@gmail.com>
2014-11-24 13:22:04 -0500

    can't figure out the looping behavior that's going on, might have to come bak to this later

--- seperation.rb
+++ seperation.rb
@@ -5,34 +5,41 @@ require 'pry'
 def degrees_of_seperation(username1, username2)
 	found = false
 	degrees = 1
-
-	puts username1
+	looked = []
 
 	# if username1 == username2
 	# 	return 'Hey, that\'s you'
 	# end
+	if(!found)
+		following = HTTParty.get("https://api.github.com/users/#{username1}/following?access_token=4bf673941f076ccdd53ebd9a4cc3fc3170ea9b3d")
+		following.each do |follow|
+			if follow["login"].downcase == username2.downcase
+				found = true
+				if(found)
+					break
+				end
+			end
+		end
 
-	following = HTTParty.get("https://api.github.com/users/#{username1}/following?access_token=4bf673941f076ccdd53ebd9a4cc3fc3170ea9b3d")
-	if username1 == 'sean-west'
-		puts 'huh'
-	end
-	following.each do |follow|
-		# puts follow["login"]
-		if follow["login"].downcase == username2.downcase
-			found = true
+
+		following.each do |following2|
+			puts looked
+			unless looked.include? following2["login"]
+				looked.push(following2["login"])
+				degrees_of_seperation(following2["login"],username2)
+			end
+		end
+		if(!found)
+			degrees +=1
 		end
 	end
 
-	if(!found)
-		following.each do |follow2|
-			# puts follow2["login"]
-			degrees_of_seperation(follow2["login"],username2)
-			end
-		degrees += 1
-	else
-		return "something should be returned if you find someone with #{degrees}"
-	end
+	if(found)
+		return "#{degrees} degree(s) of seperation"
 
+	else
+		return degrees
+	end
 
 end
 
