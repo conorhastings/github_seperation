9b5e067f4920fcf9d5d9644367d618fd4a30e3c8
conorhastings <hastings.conorm@gmail.com>
2014-11-24 13:32:42 -0500

    deleting stuff, another approach in mind

--- seperation.rb
+++ seperation.rb
@@ -6,32 +6,15 @@ def degrees_of_seperation(username1, username2)
 	found = false
 	degrees = 1
 	looked = []
+	next = 0
+
 
 	# if username1 == username2
 	# 	return 'Hey, that\'s you'
 	# end
 	if(!found)
 		following = HTTParty.get("https://api.github.com/users/#{username1}/following?access_token=4bf673941f076ccdd53ebd9a4cc3fc3170ea9b3d")
-		following.each do |follow|
-			if follow["login"].downcase == username2.downcase
-				found = true
-				if(found)
-					break
-				end
-			end
-		end
-
-
-		following.each do |following2|
-			puts looked
-			unless looked.include? following2["login"]
-				looked.push(following2["login"])
-				degrees_of_seperation(following2["login"],username2)
-			end
-		end
-		if(!found)
-			degrees +=1
-		end
+		
 	end
 
 	if(found)
