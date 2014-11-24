46e5969334fd006b67ae0c388cf9d4d9bc17ba05
conorhastings <hastings.conorm@gmail.com>
2014-11-24 12:20:15 -0500

    rate limit issues wonder if Ic an find a way to do this without loops

--- seperation.rb
+++ seperation.rb
@@ -1,21 +1,36 @@
 require 'httparty'
+require 'pry'
 
 
 def degrees_of_seperation(username1, username2)
 	found = false
-	degrees = 0
+	degrees = 1
 
 	if username1 == username2
 		return 'Hey, that\'s you'
 	end
 
-	following = HTTParty.get("https://api.github.com/users/#{username1}/following/")
+	following = HTTParty.get("https://api.github.com/users/#{username1}/following")
+	following.each do |follow|
+		puts follow
+		if follow["login"].downcase == username2.downcase
+			found = true
+
+		end
+	end
 
 	if(!found)
-	#do something recursives
+		degrees += 1
+		following.each do |follow|
+			degrees_of_seperation(follow["login"],username2)
+		end
+
+	else
+		return "something should be returned if you find someone with #{degrees}"
 	end
 
 
 end
 
 
+binding.pry
