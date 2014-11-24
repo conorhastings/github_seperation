61632f834a88d00996260a439bbf7cc265b14b9b
conorhastings <hastings.conorm@gmail.com>
2014-11-24 12:46:43 -0500

    version control

--- seperation.rb
+++ seperation.rb
@@ -6,25 +6,29 @@ def degrees_of_seperation(username1, username2)
 	found = false
 	degrees = 1
 
-	if username1 == username2
-		return 'Hey, that\'s you'
-	end
+	puts username1
 
-	following = HTTParty.get("https://api.github.com/users/#{username1}/following")
+	# if username1 == username2
+	# 	return 'Hey, that\'s you'
+	# end
+
+	following = HTTParty.get("https://api.github.com/users/#{username1}/following?access_token=4bf673941f076ccdd53ebd9a4cc3fc3170ea9b3d")
+	if username1 == 'sean-west'
+		puts 'huh'
+	end
 	following.each do |follow|
-		puts follow
+		# puts follow["login"]
 		if follow["login"].downcase == username2.downcase
 			found = true
-
 		end
 	end
 
 	if(!found)
+		following.each do |follow2|
+			# puts follow2["login"]
+			degrees_of_seperation(follow2["login"],username2)
+			end
 		degrees += 1
-		following.each do |follow|
-			degrees_of_seperation(follow["login"],username2)
-		end
-
 	else
 		return "something should be returned if you find someone with #{degrees}"
 	end
